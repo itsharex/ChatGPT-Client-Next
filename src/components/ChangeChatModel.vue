06ccca43fab4bc955d0b11ce454a8288ae4be3f8
zhuzhaofeng <zhuzhaofeng@aliyun.com>
2023-04-23 00:24:35 +0800

    🌟feat(tips): 切换GPT4 提示

--- src/components/ChangeChatModel.vue
+++ src/components/ChangeChatModel.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts">
+<script setup lang="tsx">
+import { Button, Notification } from '@arco-design/web-vue'
+
 import { useConfigStore } from '@/store/config'
 import type { ChatModel } from '@/types/chat'
 
 const configStore = useConfigStore()
 const handleChangeMode = (value: string | number | Record<string, any>) => {
+  if (String(value).indexOf('gpt-4') > -1) {
+    Notification.clear()
+    Notification.warning({
+      title: '提示',
+      closeIconElement: () => <Button size="mini">关闭</Button>,
+      duration: 0,
+      content: () => (
+        <div class="whitespace-pre-wrap flex flex-col gap-y-1 leading-5">
+          <strong>gpt-4成本昂贵</strong>
+          <span>问题字数应该保持在1000token以下(大约500个汉字)</span>
+          <span>
+            尽量让ai一次性回复完。如果AI回答的内容过长，会造成内容截断，可以让AI继续回答，但是会消耗积分。
+          </span>
+        </div>
+      ),
+      closable: true
+    })
+  }
   configStore.changeChatModelAction(value as ChatModel)
 }
 </script>
