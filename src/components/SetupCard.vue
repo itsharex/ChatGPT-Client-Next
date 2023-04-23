dcc1c4f8eb97c15eb47037699d9aadbb63df602a
zhuzhaofeng <zhuzhaofeng@aliyun.com>
2023-04-24 00:21:05 +0800

    🐛fix(card): 修复卡密输入错误

--- src/components/SetupCard.vue
+++ src/components/SetupCard.vue
@@ -5,8 +5,11 @@ const configStore = useConfigStore()
 const card = ref('')
 const loading = computed(() => configStore.setupCardLoading)
 
-const handleInputCard = (value: string) => {
-  configStore.setupCardAction(value)
+const handleInputCard = () => {
+  if (card.value === configStore.card) {
+    return
+  }
+  configStore.setupCardAction(card.value)
 }
 
 onMounted(() => {
@@ -16,26 +19,37 @@ onMounted(() => {
 
 <template>
   <a-spin :loading="loading">
-    <a-input-password
+    <a-input
       :error="!configStore.cardInfo && !!configStore.card"
       v-model="card"
+      type="password"
       @blur="handleInputCard"
       allow-clear
       placeholder="请输入积分卡"
     >
-      <template v-if="!configStore.cardInfo && configStore.card" #suffix>
-        <a-tooltip
-          content="您的积分不足、积分卡已过期或输入错误。请检查并重新输入。"
-          position="bottom"
-          :content-style="{
-            maxWidth: '240px',
-            'margin-right': '16px'
-          }"
-        >
-          <icon-info-circle class="cursor-pointer text-danger" />
-        </a-tooltip>
+      <template #suffix>
+        <div class="flex items-center">
+          <a-typography-paragraph
+            v-if="card"
+            class="mb-0"
+            copyable
+            :copy-text="card"
+          >
+          </a-typography-paragraph>
+          <a-tooltip
+            v-if="!configStore.cardInfo && configStore.card"
+            content="您的积分不足、积分卡已过期或输入错误。请检查并重新输入。"
+            position="bottom"
+            :content-style="{
+              maxWidth: '240px',
+              'margin-right': '16px'
+            }"
+          >
+            <icon-info-circle class="cursor-pointer text-danger" />
+          </a-tooltip>
+        </div>
       </template>
-    </a-input-password>
+    </a-input>
   </a-spin>
 </template>
 
